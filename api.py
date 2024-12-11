de54c79448bcd307536850969ca21b2a9789e7e8
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-12-11 20:34:21 +0700

    Replaced `post` for `get`

--- api.py
+++ api.py
@@ -9,11 +9,9 @@ from logging import *
 __doc__ = """
 GET /api/help/ - returns this text
 
-POST /api/packages/?field=<field> - returns this field from each package
+GET /api/package_by_field/?field=<field>&value=<value> - returns package, where <field>=<value>. Supported fields: id, name, version, *
 
-POST /api/package_by_field/?field=<field>&value=<value> - returns package, where <field>=<value>. Supported fields: id, name, version, *
-
-POST /api/package/?name=<name>&version=<version> - returns package, where name=<name> and version=<version>
+GET /api/package/?name=<name>&version=<version> - returns package, where name=<name> and version=<version>
 """
 
 basicConfig(
@@ -28,46 +26,33 @@ app = FastAPI()
 async def help() -> PlainTextResponse:
     return PlainTextResponse(__doc__)
 
-@app.post('/api/packages/')
-async def get_packages(field: str) -> JSONResponse:
-    if field in {'id', 'name', 'author', 'url'}:
-        async with connect(DB_PATH) as db:
-            result = await (await db.execute(f'''
-                SELECT {field} FROM packages
-            ''')).fetchall()
-            return JSONResponse({
-                'packages':[dict([(field, value[0])]) for value in result]
-            })
-    else:
-        return JSONResponse({'detail':'Field must be one of: name, id, author, url'}, status_code=404)
-
-@app.post('/api/package_by_field/')
+@app.get('/api/package_by_field/')
 async def get_package_by_field(field: str, value: str) -> JSONResponse:
     if field in ['id', 'name', 'author']:
         async with connect(DB_PATH) as db:
             result: list[int, str] = await (await db.execute(f'''
-                SELECT id, author, version, url FROM packages
+                SELECT * FROM packages
                 WHERE {field}=?
             ''', (value,))).fetchone()
             if result:
                 return JSONResponse({
-                    'package':dict(zip(['id', 'author', 'version', 'url'], result))
+                    'package':dict(zip(['id', 'name', 'author', 'version', 'url'], result))
                 })
             else:
                 return JSONResponse({'detail':'Package not found'}, status_code=404)
     else:
         return JSONResponse({'detail':'Field must be one of: name, id, author'}, status_code=404)
 
-@app.post('/api/package/')
+@app.get('/api/package/')
 async def get_package(name: str, version: str) -> JSONResponse:
     async with connect(DB_PATH) as db:
         result: list[int, str] = await (await db.execute(f'''
-            SELECT id, author, version, url FROM packages
+            SELECT * FROM packages
             WHERE name=? AND version=?
         ''', (name, version))).fetchone()
         if result:
             return JSONResponse({
-                'package':dict(zip(['id', 'author', 'version', 'url'], result))
+                'package':dict(zip(['id', 'name', 'author', 'version', 'url'], result))
             })
         else:
             return JSONResponse({'detail':'Package not found'}, status_code=404)
