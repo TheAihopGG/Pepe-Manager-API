2818a1fa18a92595a6f7133ec81a738e0015583b
TheAihopGG <theaihopgg@gmail.com>
2025-03-29 23:01:46 +0700

    feat: basic

--- backend/routers/private_packages/views.py
+++ backend/routers/private_packages/views.py
@@ -3,21 +3,13 @@ from urllib.parse import urlparse
 from fastapi import APIRouter, HTTPException, status, Depends
 from fastapi.responses import JSONResponse
 from fastapi.requests import Request
-from backend.core import cfg
-
-
-def check_auth(request: Request):
-    if not request.headers.get("X-Token"):
-        raise HTTPException(status_code=403)
+from core import cfg
+from .schemas import Schemas
 
 
 class PrivatePackagesRouter(APIRouter):
     def __init__(self):
         super().__init__(prefix=cfg["routers_prefixes"]["private_packages"])
-
-        @self.get("/", dependencies=[Depends(check_auth)])
-        async def test() -> JSONResponse:
-            return JSONResponse({"success": True})
 
 
 router = PrivatePackagesRouter()
--- backend/routers/public_packages/views.py
+++ backend/routers/public_packages/views.py
@@ -1,16 +1,12 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from fastapi.requests import Request
-from backend.core import cfg
+from core import cfg
 
 
 class PublicPackagesRouter(APIRouter):
     def __init__(self):
         super().__init__(prefix=cfg["routers_prefixes"]["public_packages"])
-
-        @self.get("/")
-        async def test() -> JSONResponse:
-            return JSONResponse({"success": True})
 
 
 router = PublicPackagesRouter()
