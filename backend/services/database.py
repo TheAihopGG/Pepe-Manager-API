8d07a92086b1cd446f1f356286ba1d5b5a794dbb
TheAihopGG <theaihopgg@gmail.com>
2025-03-29 23:03:20 +0700

    fix: removed unused funcs

--- backend/services/database.py
+++ backend/services/database.py
@@ -4,43 +4,10 @@ from typing import Callable, Awaitable
 
 
 class Database:
-    def memory_connection(function):
-        @wrapper(function)
-        async def wrapper(self):
-            session = await aiosqlite.connect(":memory:")
-            await Database.create_tables(session)
-            return await function(self, session=session)
-
-        return wrapper
-
     @staticmethod
-    def rollback_on_error[**P, R](
-        function: Callable[P, Awaitable[R]],
-    ) -> Callable[P, Awaitable[R]]:
-        @wraps(function)
-        async def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-            session = kwargs.get("session")
-            if not session or not isinstance(session, aiosqlite.Connection):
-                raise ValueError(
-                    "Session must be an aiosqlite.Connection and provided as keyword argument"
-                )
-            try:
-                result = await function(*args, **kwargs)
-            except Exception as err:
-                await session.execute("ROLLBACK TRANSACTION")
-                raise err
-            else:
-                await session.execute("COMMIT TRANSACTION")
-                return result
-
-        return wrapper
-
-    @staticmethod
-    @rollback_on_error
     async def create_tables(*, session: aiosqlite.Connection):
-        return await session.executescript(
+        await session.executescript(
             """
-            BEGIN TRANSACTION;
             CREATE TABLE IF NOT EXISTS packages (
                 id INTEGER PRIMARY KEY,
                 name VARCHAR(32),
@@ -54,17 +21,17 @@ class Database:
             CREATE INDEX IF NOT EXISTS package_idx ON packages(id);
             """
         )
+        await session.commit()
 
     @staticmethod
-    @rollback_on_error
     async def drop_tables(*, session: aiosqlite.Connection):
         await session.executescript(
             """
-            BEGIN TRANSACTION;
             DROP TABLE IF EXISTS packages;
             DROP INDEX IN EXISTS package_idx;
             """
         )
+        await session.commit()
 
 
 __all__ = ("Database",)
