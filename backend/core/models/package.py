83e163a0456286dc8af4b789bb1006fb564208a1
TheAihopGG <theaihopgg@gmail.com>
2025-03-29 13:32:33 +0700

    feat: united

--- backend/core/models/package.py
+++ backend/core/models/package.py
@@ -2,10 +2,11 @@ from typing import NamedTuple
 
 
 class Package(NamedTuple):
-    id: int
-    name: str
-    description: str
-    version: str
-    author_name: str
-    created_at: int
-    updated_at: int
+    id: int | None = None
+    name: str | None = None
+    description: str | None = None
+    version: str | None = None
+    author_name: str | None = None
+    data: bytes | None = None
+    created_at: int | None = None
+    updated_at: int | None = None
--- backend/core/models/package_data.py
+++
@@ -1,7 +0,0 @@
-from typing import NamedTuple
-
-
-class PackageData(NamedTuple):
-    id: int
-    package_id: int
-    data: bytes
